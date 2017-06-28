2b016bcc9d49cb0d7c8e720927355d6427595f0b
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-28 13:31:10 +0900

    Redirect after POST, and show all items in template

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -49,15 +49,25 @@ class HomePageTest(TestCase):
         # request = HttpResponse()
         # request.method = 'POST'
         # request.POST['item_text'] = '신규 작업 아이템'
-        #
+        # #
         # response = home_page(request)
         #
         # self.assertIn('신규 작업 아이템', response.content.decode())
         response = self.client.post('/', data={
-            'item_text': '신규 작업 아이템',
+            'item_text': 'A new list item',
             })
-        self.assertIn('신규 작업 아이템', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+        # self.assertIn('A new list item', response.content.decode())
+        # self.assertTemplateUsed(response, 'lists/home.html')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
     # def test_uses_home_template(self):
     #     response = self.client.get('/')
@@ -68,4 +78,19 @@ class HomePageTest(TestCase):
     #         'item_text': '신규 작업 아이템',
     #         })
     #     self.assertIn('신규 작업 아이템', response.content.decode())
+
+    def test_home_page_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
+
 
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -1,13 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 
 def home_page(request):
     # if request.method == 'POST':
     #     return HttpResponse(request.POST['item_text'])
     # return render(request, 'lists/home.html')
+    # item = Item()
+    # item.text = request.POST.get('item_text', '')
+    # item.save()
+
+    if request.method == 'POST':
+        # new_item_text = request.POST['item_text']
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+    # else:
+    #     new_item_text = ''
+
+    items = Item.objects.all()
     context = {
-        'new_item_text': request.POST.get('item_text', ''),
+        'items': items,
         }
     return render(request, 'lists/home.html', context)
 
