703b3532f770d713c6f28fcef61891229ee75326
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-28 10:40:59 +0900

    about to refactoring

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -7,7 +7,7 @@ def home_page(request):
     #     return HttpResponse(request.POST['item_text'])
     # return render(request, 'lists/home.html')
     context = {
-        'new_item_text': request.POST.get['item_text'],
+        'new_item_text': request.POST.get('item_text', ''),
         }
     return render(request, 'lists/home.html', context)
 
